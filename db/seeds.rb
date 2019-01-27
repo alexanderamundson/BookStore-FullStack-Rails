5a07a653a236e325de20d9f8eee4435a0d886d5d
alexanderamundson <alex.amundson987@gmail.com>
2019-01-27 23:43:54 +0000

    Finished A2 - updated layout of Product List

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 #---
 # Excerpted from "Agile Web Development with Rails 5.1",
 # published by The Pragmatic Bookshelf.
