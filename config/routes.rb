8bb9523c843a5284159f0e2e458fe42a0f53f29b
alexanderamundson <alex.amundson987@gmail.com>
2019-01-28 18:51:38 +0000

    Ensured user input into new product form meets requirments (Part B1)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'products#show'
+  root 'products#index'
 end
