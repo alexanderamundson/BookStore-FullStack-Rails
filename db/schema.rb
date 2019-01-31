e8aba23c11cbb2355914b5d02189ac4bf8505807
alexanderamundson <alex.amundson987@gmail.com>
2019-01-31 19:50:36 +0000

    added carts_controller (Iteration D1)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_192651) do
+ActiveRecord::Schema.define(version: 2019_01_31_191650) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
