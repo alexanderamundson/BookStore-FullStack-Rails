e3ef34b530d2cf47e5f7376ebd6a9fe54f7633f8
alexanderamundson <alex.amundson987@gmail.com>
2019-02-02 20:45:39 +0000

    Created button in store/index.html to add a new line_item to cart (D3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :carts
   root 'store#index', as: 'store_index'
-  
+  resources :carts
   resources :products
+  resources :line_items
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
