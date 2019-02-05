3d5bbd1e3b6ef6c88c489f93552a7f7a3a0c92ca
alexanderamundson <alex.amundson987@gmail.com>
2019-02-05 22:10:10 +0000

    Fixed destroy product bug, created My Shopping Cart link, and finished playtime (2.1)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,17 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.order(:title)
+    @products = Product.order(:popularity).reverse_order
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+      if session[:counter] > 5
+        flash.now[:index]="You visited the catalog #{session[:counter]}
+           times".pluralize(session[:counter])+" without buying anything!... Come on!"
+        render :index  
+    
+    
+      end
+    end    
   end
-  
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :products
   resources :line_items
   
+  get 'carts/:id', to: 'carts#show', as: 'myCart' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
