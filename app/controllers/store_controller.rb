704793dfdf8b0a63280cd9d52dec72b8a977da12
alexanderamundson <alex.amundson987@gmail.com>
2019-02-07 01:08:44 +0000

    Added to StoreController to make homepage load correctly

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:index]
+  
   def index
     @products = Product.order(:popularity).reverse_order
     if session[:counter].nil?
