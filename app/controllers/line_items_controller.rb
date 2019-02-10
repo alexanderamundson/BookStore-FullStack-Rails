64792106b6db3eb88102081b475a79b1e77aa4e7
alexanderamundson <alex.amundson987@gmail.com>
2019-02-10 20:37:08 +0000

    Ensured users can only see their own cart and can't destroy others' lineitems

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -73,6 +73,9 @@ class LineItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
+      if @line_item.id != session[:line_item_id]
+        invalid_line_item
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
