9aa8afce31e9924f86ea552b25a216192e469129
alexanderamundson <alex.amundson987@gmail.com>
2019-02-12 22:07:23 +0000

    Made app show each product's popularity on homepage

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -33,11 +33,13 @@ class StoreController < ApplicationController
   end
   
   private 
+  
     def sort_by
        %w(title
           price
           popularity).include?(params[:sort_by]) ? params[:sort_by] : 'popularity'
     end
+  
     def order
        %w(asc desc).include?(params[:order]) ? params[:order] : 'asc'
     end
